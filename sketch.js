8bbd4a38b90f294819a0e66d89c4e7bdecf08834
Aarush Singhal <nehajain.29@gmail.com>
2020-07-02 18:53:42 +0530

    Aarush

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,9 +34,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(100,100,150,PI/3);
 
     bird = new Bird(100,100);
 
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -61,4 +64,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+    chain.display();
 }
